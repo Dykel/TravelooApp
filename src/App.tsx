d9b2c27200276d06271ff5a8f3b691d63a603ec3
Joseph Jonathan COLLET <dykelaway@gmail.com>
2025-05-22 20:32:17 +0400

    Add React import to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ScooterDetails from './pages/ScooterDetails';
+import Bookings from './pages/Bookings';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/scooter/:id" element={<ScooterDetails />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
